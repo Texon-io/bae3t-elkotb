42e7ef40ae29e53c424bc524e67f9e1d672becca
ramez <ramez.elzoz74@gmail.com>
2025-11-13 12:43:32 +0200

    Editing the structure of about section

---
+++ src/components/atoms/AboutImg.jsx
@@ -0,0 +1,9 @@
+function AboutImg() {
+  return (
+    <figure className="w-xs">
+      <img src="/images/about-img.png" alt="about" />
+    </figure>
+  );
+}
+
+export default AboutImg;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,31 +1,14 @@
 import Button from "../atoms/Button";
 import SecHeading from "../atoms/SecHeading";
 import AboutList from "../molecules/AboutList";
+import AboutSec from "../organisms/AboutSec";
 import HeroSec from "../organisms/HeroSec";
 
 function HomePage() {
   return (
     <div>
       <HeroSec />
-      <section id="about" className="about pt-2 pb-6 px-8 h-fit">
-        <SecHeading>من أنا</SecHeading>
-
-        <div className=" py-3 content-holder flex flex-col-reverse sm:flex-row justify-center items-center gap-12 sm:gap-24 md:gap-52">
-          <figure className="w-xs">
-            <img src="/images/about-img.png" alt="about" />
-          </figure>
-          <div className="about-txt w-full sm:w-1/2 md:w-1/3  flex flex-col gap-12">
-            <p className="text-2xl font-semibold">
-              أنا بائعة الكتب،بدأت من حبّي للقراءة... وقررت أشارككم الكتب اللي
-              لمست قلبي.
-            </p>
-
-            <AboutList />
-
-            <Button>أنضم لنا</Button>
-          </div>
-        </div>
-      </section>
+      <AboutSec />
     </div>
   );
 }
